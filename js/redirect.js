9d4863afb73979054b56a1d752d0264983dd2170
Tojefin <tojefin@gmail.com>
2023-12-16 17:01:31 +0400

    bg fix

--- js/redirect.js
+++ js/redirect.js
@@ -1,11 +1,12 @@
 chrome.storage.sync.get("defaultOptions", function (defaults) {
 	chrome.storage.sync.get(defaults.defaultOptions, function (options) {
-		console.log(options)
+		document.head.innerHTML += `<style> body { background: ${options.bgColor}} </style>`
 
 		window.location.replace(options.url || 'options.html')
 		if (options.focus) {
-			window.history.pushState('', '', '');
+			window.history.pushState('', '', '')
 		}
 
+		console.log(options)
 	})
 })
